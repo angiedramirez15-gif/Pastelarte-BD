984f2995a86f3fe2925bc9867a77dd06565c3120
Daniela Ramirez <angie.dramirez15@gmail.com>
2025-12-07 13:25:58 -0500

    agrego deletes

---
+++ deletes.sql
@@ -0,0 +1,17 @@
+-- Borrar un cliente específico
+DELETE FROM clientes
+WHERE idcliente = 8;
+
+DELETE FROM clientes
+WHERE idcliente = 8;
+
+
+-- Borrar pedidos 
+DELETE FROM pedidos
+WHERE fecha < '2024-01-01';
+
+DELETE FROM pedidos
+WHERE idpedido=1;
+
+DELETE FROM pedidos
+WHERE idpedido=2;
